55bdb7d1f2dd5f30558e3ee5cb619f08982ed1de
julioc74 <cesarng74@gmail.com>
2024-08-04 23:24:26 +0900

    colocando labeles y agrandando tamanio de fuentes

--- suma_dos_numeros.py
+++ suma_dos_numeros.py
@@ -11,24 +11,37 @@ def sumar():
     
     resultado.config(text= f'La suma es : {suma}')
 
+# Colocando etiqueta de primer numero
+primer_numero_etiqueta = tk.Label(root,
+                                  font= ("Times", 20),
+                                  text= "Ingrese primer numero")
+primer_numero_etiqueta.pack()
 
 # Cambiando la fuente y el tamanio 
 primer_numero_entrada = tk.Entry(root, 
                                  font= ("Times", 20))
 primer_numero_entrada.pack()
 
+# Colocando etiqueta de segundo numero 
+segundo_numero_etiqueta = tk.Label(root,
+                                  font= ("Times", 20),
+                                  text= "Ingrese segundo numero")
+segundo_numero_etiqueta.pack()
 
 # Cambiando la fuente y el tamanio
 segundo_numero_entrada = tk.Entry(root, 
                                   font= ("Times", 20))
 segundo_numero_entrada.pack()
 
+# Cambiando la fuente y tamanio
 boton = tk.Button(root,
                   text= "Sumar",
+                  font= ("Times", 20),
                   command= sumar)
 boton.pack() 
 
 resultado = tk.Label(root,
+                     font= ("Times", 20),
                      text= "")
 resultado.pack()
 
