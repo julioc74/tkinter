5d8616347bbb6f24570cbe93895f51ab33acc1d9
julioc74 <cesarng74@gmail.com>
2024-08-11 20:38:57 +0900

    terminando el programa con boton, su funcion y label de resultado

--- dolares_a_soles.py
+++ dolares_a_soles.py
@@ -5,6 +5,14 @@ root.geometry("500x500")
 root.resizable(width= "False", height= "False")
 root.title("CONVERTIDOR DE DOLARES A SOLES")
 
+def convertir():
+    
+    cantidad_dolares = float(entrada_dolares.get())
+    cambio = float(entrada_cambio.get())
+    total_soles = cantidad_dolares * cambio 
+    
+    resultado.config(text= f'La cantidad en soles es {total_soles}')
+
 etiqueta_dolares = tk.Label(root,
                             font= ("Arial", 20),
                             text= "Ingrese cantidad en dolares",
@@ -28,6 +36,20 @@ etiqueta_cambio.pack()
 entrada_cambio = tk.Entry(root,
                           font= ("Arial", 20))
 entrada_cambio.pack()
+
+boton = tk.Button(root,
+                  text= "convertir",
+                  font= ("Arial", 20),
+                  command= convertir,
+                  )
+
+boton.pack()
+
+resultado = tk.Label(root,
+                     font= ("Arial", 20),
+                     text= "")
+
+resultado.pack()
 
 
 root.mainloop()
